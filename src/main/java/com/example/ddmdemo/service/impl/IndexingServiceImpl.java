10b54df746c83db7cce9a11cfc3749c1818a10d2
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-10-23 16:11:40 +0200

    Added postgres and language and mime type detection.

--- src/main/java/com/example/ddmdemo/service/impl/IndexingServiceImpl.java
+++ src/main/java/com/example/ddmdemo/service/impl/IndexingServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.ddmdemo.service.impl;
 
 import com.example.ddmdemo.exceptionhandling.exception.LoadingException;
+import com.example.ddmdemo.exceptionhandling.exception.StorageException;
 import com.example.ddmdemo.indexmodel.DummyIndex;
 import com.example.ddmdemo.indexrepository.DummyIndexRepository;
+import com.example.ddmdemo.model.DummyTable;
+import com.example.ddmdemo.respository.DummyRepository;
 import com.example.ddmdemo.service.interfaces.FileService;
 import com.example.ddmdemo.service.interfaces.IndexingService;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.tika.Tika;
+import org.apache.tika.language.detect.LanguageDetector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,18 +27,37 @@ public class IndexingServiceImpl implements IndexingService {
 
     private final DummyIndexRepository dummyIndexRepository;
 
+    private final DummyRepository dummyRepository;
+
     private final FileService fileService;
+
+    private final LanguageDetector languageDetector;
+
 
     @Override
     public String indexDocument(MultipartFile documentFile) {
+        var newEntity = new DummyTable();
         var newIndex = new DummyIndex();
-        newIndex.setTitle(
-            Objects.requireNonNull(documentFile.getOriginalFilename()).split("\\.")[0]);
-        newIndex.setContent(extractDocumentContent(documentFile));
+
+        var title = Objects.requireNonNull(documentFile.getOriginalFilename()).split("\\.")[0];
+        newIndex.setTitle(title);
+        newEntity.setTitle(title);
+
+        var documentContent = extractDocumentContent(documentFile);
+        if (detectLanguage(documentContent).equals("SR")) {
+            newIndex.setContentSr(documentContent);
+        } else {
+            newIndex.setContentEn(documentContent);
+        }
+        newEntity.setTitle(title);
 
         var serverFilename = fileService.store(documentFile, UUID.randomUUID().toString());
         newIndex.setServerFilename(serverFilename);
 
+        newEntity.setMimeType(detectMimeType(documentFile));
+        var savedEntity = dummyRepository.save(newEntity);
+
+        newIndex.setDatabaseId(savedEntity.getId());
         dummyIndexRepository.save(newIndex);
 
         return serverFilename;
@@ -47,6 +73,37 @@ public class IndexingServiceImpl implements IndexingService {
         } catch (IOException e) {
             throw new LoadingException("Error while trying to load PDF file content.");
         }
+
         return documentContent;
+    }
+
+    private String detectLanguage(String text) {
+        var detectedLanguage = languageDetector.detect(text).getLanguage().toUpperCase();
+        if (detectedLanguage.equals("HR")) {
+            detectedLanguage = "SR";
+        }
+
+        return detectedLanguage;
+    }
+
+    private String detectMimeType(MultipartFile file) {
+        var contentAnalyzer = new Tika();
+
+        String trueMimeType;
+        String specifiedMimeType;
+        try {
+            trueMimeType = contentAnalyzer.detect(file.getBytes());
+            specifiedMimeType =
+                Files.probeContentType(Path.of(Objects.requireNonNull(file.getOriginalFilename())));
+        } catch (IOException e) {
+            throw new StorageException("Failed to detect mime type for file.");
+        }
+
+        if (!trueMimeType.equals(specifiedMimeType) &&
+            !(trueMimeType.contains("zip") && specifiedMimeType.contains("zip"))) {
+            throw new StorageException("True mime type is different from specified one, aborting.");
+        }
+
+        return trueMimeType;
     }
 }
